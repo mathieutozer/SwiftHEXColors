fa2016877c4992b059060e9db750a232b164ae38
Mathieu Tozer <mathieutozer@gmail.com>
2020-02-20 17:22:21 +0100

    watchOS

--- Tests/SwiftHEXColorsTests/SwiftHEXColorsTests.swift
+++ Tests/SwiftHEXColorsTests/SwiftHEXColorsTests.swift
@@ -20,6 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+#if !os(watchOS)
 import XCTest
 @testable import SwiftHEXColors
 
@@ -158,3 +159,4 @@ class SwiftHEXColorsTests: XCTestCase {
         XCTAssertNil(wrongColor06)
     }
 }
+#endif
